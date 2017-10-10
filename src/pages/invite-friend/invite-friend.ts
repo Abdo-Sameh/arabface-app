4ef0a668193f285e2551f7100b8ae8c90e674fb8
Abdo-Sameh <abdo.sameh2000@gmail.com>
2017-10-10 11:11:57 +0200

    invite friend and search

--- src/pages/invite-friend/invite-friend.ts
+++ src/pages/invite-friend/invite-friend.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import { RemoteServiceProvider} from './../../providers/remote-service/remote-service';
 
 
@@ -18,29 +18,55 @@ export class InviteFriendPage {
   userId
   friends
   page
-  constructor(public navCtrl: NavController, public navParams: NavParams, public remoteService :RemoteServiceProvider) {
+  search
+  invited :boolean
+  constructor(public navCtrl: NavController, public navParams: NavParams, public remoteService :RemoteServiceProvider, public toastCtrl :ToastController) {
     this.userId = localStorage.getItem('userDataID').replace(/[^0-9]/g, "");
-    this.page = this.navParams.get('page');
+    this.page = navParams.get("page");
     this.friendsListApiCall(this.userId, this.userId, '');
   }
-
   ionViewDidLoad() {
     console.log('ionViewDidLoad InviteFriendPage');
   }
-  friendsListApiCall(the_userid, id, term){
-    this.remoteService.friendsListApiCall(the_userid, id, term).subscribe(res => {
-      this.friends = res;
+  friendsListApiCall(the_userid, id, term)
+  {
+    this.remoteService.friendsListApiCall(the_userid, id, term).subscribe(res =>
+      {
+        this.friends = res
+
+      for(let i = 0; i < res.length; i++)
+      {
+          console.log(res[i].id)
+          this.remoteService.isInvitedPage(this.page.id, res[i].id, this.userId).subscribe(res2 =>
+            {
+              console.log(res[i].id);
+              if(res2.status == 1)
+                res[i].invited = true
+              else
+                res[i].invited = false
+          })
+      }
+      console.log(this.friends);
+    });
+  }
+
+
+  inviteFriendTolikepage(invited_id, index){
+    this.remoteService.inviteFriendTolikepage(this.userId, this.page.id, invited_id).subscribe(res =>{
       console.log(res);
+      if(res.status == 1)
+        this.friends[index].invited = true;
+      else if(res.status == 0 && res.message == "user-already-liked"){
+        let toast = this.toastCtrl.create({
+          message: 'User already liked',
+          duration: 3000,
+          position: 'top'
+        });
+        toast.present();
+      }
     });
   }
-  isInvitedPage(user){
-    this.remoteService.isInvitedPage(this.page.id, user, this.userId).subscribe(res => {
-      if(res.status == true)
-        return true;
-      else
-        return false;
-    });
-  }
+
   back(){
     this.navCtrl.pop();
   }
