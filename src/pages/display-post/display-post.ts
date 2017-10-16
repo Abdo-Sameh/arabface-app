90fe1486b191605d084057e0b081fd4841fa8864
abdelrahman <dev.abdelrahman1995@gmail.com>
2017-10-16 15:07:56 +0200

    posts of events and profile

--- src/pages/display-post/display-post.ts
+++ src/pages/display-post/display-post.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController,PopoverController , NavParams ,LoadingController ,AlertController} from 'ionic-angular';
+import { IonicPage, NavController,PopoverController , NavParams ,LoadingController ,AlertController,ToastController} from 'ionic-angular';
 import { RemoteServiceProvider } from './../../providers/remote-service/remote-service';
 
 /**
@@ -20,6 +20,7 @@ export class DisplayPostPage {
       'reply' :''
       }
       likes;
+      hiddenPost
       likeNumbers;
       userData;
       show=true
@@ -30,7 +31,7 @@ export class DisplayPostPage {
       userAvatar = localStorage.getItem('userAvatar').slice(8,-1);
       
       feed = { 'feedid' :""}
-  constructor(public navCtrl: NavController,public popOver : PopoverController  , public navParams: NavParams ,public alert:AlertController,public loadingCtrl: LoadingController, public remoteService : RemoteServiceProvider) {
+  constructor(public navCtrl: NavController,public popOver : PopoverController  ,public toast:ToastController , public navParams: NavParams ,public alert:AlertController,public loadingCtrl: LoadingController, public remoteService : RemoteServiceProvider) {
       this.post=this.navParams.get('post')
     console.log(this.post)
 
@@ -160,6 +161,151 @@ sharePost(feedid,userID=this.userId)
 
 }
 
+editPost()
+{
+  $(document).on('click','.comment-edit',function(){
+    $(this).parent().prev().find('.input-group').show();
+
+  })
+  $(document).on('click','.cancel-edit',function(){
+    $(this).parent().hide();
+
+  })
+}
+ConfirmEditPost(text,feedid)
+{
+    this.remoteService.editPost(text,feedid,this.userId).subscribe((data) => {console.log(data)})
+}
+
+savePost(feedid)
+{
+let toast = this.toast.create({
+  message: 'this post has saved !',
+  duration : 2000,
+  cssClass: 'alert'
+});
+toast.present();
+this.remoteService.saveItem('feed',feedid,this.userId).subscribe(res => {
+  console.log(res)
+})
+}
+donotLikePost(feedid,index,userID=this.userId)
+{
+  this.remoteService.hidePost(feedid,userID).subscribe(res => {
+    this.hiddenPost = res.status
+    if(res.status == 1 )
+    {
+      this.post.splice(index,1)
+
+      let toast = this.toast.create({
+        message: 'This post will no longer show to you',
+        duration : 2000
+      });
+      toast.present();
+    }
+  })
+}
+deletePost(feedid,index,userID=this.userId)
+{
+  let alert = this.alert.create({
+    title: 'Delete',
+    message: 'Do you want to delete this post?',
+
+    buttons: [
+      {
+        text: 'Ok',
+        handler: () => {
+
+          this.remoteService.removePost(feedid,userID).subscribe(res => {
+            if(res.status == 1 )
+            {
+              let toast = this.toast.create({
+                message: 'You deleted this post ',
+                duration : 2000
+              });
+              toast.present();
+              this.navCtrl.pop()
+            }
+          })
+        }
+      },
+      {
+        text: 'Cancel',
+        role: 'cancel',
+        handler: () => {
+        }
+      }
+    ]
+  });
+  alert.present();
+}
+deleteComment(commentId)
+{
+  let alert = this.alert.create({
+    title: 'Delete',
+    message: 'Do you want to delete comment?',
+    buttons: [
+      {
+        text: 'Ok',
+        handler: () => {
+          this.remoteService.removeComment(commentId,this.userId).subscribe(res => {
+            if(res.status == 1 )
+            {
+
+              let toast = this.toast.create({
+                message: 'You deleted this comment ',
+                duration : 2000
+
+              });
+              toast.present();
+            }
+          })
+        }
+      },
+      {
+        text: 'Cancel',
+        role: 'cancel',
+        handler: () => {
+        }
+      }
+    ]
+  });
+  alert.present();
+
+
+}
+turnNotifications(feedid,index,feedType,userID=this.userId)
+{
+  if(feedType == true)
+  {
+    this.remoteService.unsubscribePost(feedid,userID).subscribe((data) => { console.log(data)
+    if(data.status == 1)
+    {
+      this.post.has_subscribed = !feedType
+    }
+    })
+    
+  }else {
+    this.remoteService.subscribePost(feedid,userID).subscribe((data) => { console.log(data)
+      if(data.status == 1)
+      {
+        this.post.has_subscribed = !feedType
+      }
+    })
+    
+  }
+}
+showPost(feed)
+{
+  this.postToDisplay=feed
+  console.log(this.postToDisplay)
+  this.navCtrl.push(DisplayPostPage,{'post':this.postToDisplay})
+}
+//////////////////////////////////////////
+effects()
+{
+$(this).css('background-color','grey')
+}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad DisplayPostPage');
