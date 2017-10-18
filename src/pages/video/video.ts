23e53f862593c59dacad83d2b6603571c483dfbc
Abdo-Sameh <abdo.sameh2000@gmail.com>
2017-10-18 12:09:10 +0200

    share video

--- src/pages/video/video.ts
+++ src/pages/video/video.ts
@@ -3,7 +3,7 @@ import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angu
 import { RemoteServiceProvider} from './../../providers/remote-service/remote-service';
 import { VideosPage } from '../videos/videos';
 import { EditVideoPage } from '../edit-video/edit-video';
-
+import { SocialSharing } from '@ionic-native/social-sharing';
 
 /**
  * Generated class for the VideoPage page.
@@ -20,7 +20,7 @@ export class VideoPage {
 
   video
   userId
-  constructor(public navCtrl: NavController, public navParams: NavParams, public remoteService :RemoteServiceProvider, public toastCtrl :ToastController) {
+  constructor(private socialSharing: SocialSharing, public navCtrl: NavController, public navParams: NavParams, public remoteService :RemoteServiceProvider, public toastCtrl :ToastController) {
     this.userId = localStorage.getItem('userDataID').replace(/[^0-9]/g, "");
     this.video = this.navParams.get('video');
     // this.video.code = this.video.code.substr(1);
@@ -47,6 +47,10 @@ export class VideoPage {
       toast.present();
       this.navCtrl.push(VideosPage);
     });
+  }
+  regularShare(){
+    // share(message, subject, file, url)
+    this.socialSharing.share(this.video.title, "Arabface", "assets/images/logo.png", this.video.video_url);
   }
   back(){
     this.navCtrl.pop();
